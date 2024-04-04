57ce0e488832d65d5f73516079fe06b05698a9e6
Leighton Miguel <leim56@hawaii.rr.com>
2024-04-03 16:56:35 -1000

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,8 +5,42 @@ const Index = () => {
   return (
     <>
       <div className="container text-left">
-        This is the index page.
-        <ExampleComponent />
+        <section className="py-6" style={{ marginTop: "15%" }}>
+          <div
+            className="container min-vh-50 py-6 d-flex justify-content-center align-items-center"
+            style={{ maxWidth: 1920 }}
+          >
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <div className="lc-block d-flex flex-row justify-content-center"></div>
+                <div className="lc-block mb-4">
+                  <div>
+                    <h1 className="fw-bold display-1">Programming Languages</h1>
+                  </div>
+                </div>
+                <div className="lc-block">
+                  <div>
+                    <p className="h2">Leighton Miguel</p>
+                    <p className="lead">
+                      Click the button below to view the some programming
+                      languages.
+                    </p>
+                  </div>
+                </div>
+                <div className="lc-block">
+                  <a
+                    className="btn btn-primary"
+                    href="/login"
+                    role="button"
+                    style={{ backgroundColor: "#1a7e9c" }}
+                  >
+                    To Programming Languages
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
