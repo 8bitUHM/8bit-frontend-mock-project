2b81f70ba99d101a80767a5164fe295a2508f866
Leighton Miguel <leim56@hawaii.rr.com>
2024-04-03 16:48:12 -1000

    created new page

---
+++ src/pages/ProgrammingLanguages.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const ProgrammingLanguages = () => {
+  return (
+    <>
+      <div className="container text-left">
+        This is the programming language page
+      </div>
+    </>
+  );
+};
+
+export default ProgrammingLanguages;
