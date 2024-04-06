c244f4e8043b94acaa5e038b8b76cba49cdf4851
Leighton Miguel <leim56@hawaii.rr.com>
2024-04-06 13:22:01 -1000

    Update ProgrammingLanguages.tsx

--- src/pages/ProgrammingLanguages.tsx
+++ src/pages/ProgrammingLanguages.tsx
@@ -1,23 +1,33 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, FC } from "react";
 import LoadingImage from "../components/LoadingImage";
 
+interface Image {
+  image: string;
+}
+
+interface LanguageItem {
+  name: string;
+  short_description: string;
+  images: Image[];
+}
+
 const ProgrammingLanguages = () => {
-  const [languageData, setLanguageData] = useState([]);
-  const [pageReady, setPageReady] = useState(false);
-  const [canMap, setCanMap] = useState(false);
+  const [pageReady, setPageReady] = useState<boolean>(false);
+  const [canMap, setCanMap] = useState<boolean>(false);
+  const [languageData, setLanguageData] = useState<LanguageItem[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const username = 'apiUser'
-        const password = 'apiPassword'
+        const username = "apiUser";
+        const password = "apiPassword";
         const encodedCredentials = btoa(`${username}:${password}`);
-        console.log(username,password)
+        console.log(username, password);
 
         const headers = new Headers();
         headers.append("Authorization", `Basic ${encodedCredentials}`);
-        
+
         const res = await fetch(
           `https://8bit-backend-mock-project.vercel.app/api/programming-languages`,
           { method: "GET", headers: headers }
@@ -41,28 +51,23 @@ const ProgrammingLanguages = () => {
     <>
       <div className="container text-left my-5">
         {canMap ? (
-          languageData.map((item, index) => (
+          languageData.map((item: LanguageItem, index: number) => (
             <div key={index} className="card mb-3">
               <div className="card-body">
                 <h3 className="card-subtitle">{item.name}</h3>
                 <p className="card-subtitle py-1" style={{ color: "grey" }}>
                   {item.short_description}
                 </p>
-                <div className=" card-subtitle my-2">
-                  <hr></hr>
-                  <div className="d-flex justify-content-center flex-column">
-                    <div className="pb-2">{item.text_content}</div>
-                    <div className="row justify-content-evenly pt-2">
-                      {item.images.map((image: any, index: any) => (
-                        <div key={index} className="col-12">
-                          <LoadingImage
-                            imageUri={image.image}
-                            className="img-fluid shadow-lg mb-5 bg-body rounded w-100"
-                          />
-                        </div>
-                      ))}
+                <hr />
+                <div className="row justify-content-evenly pt-2">
+                  {item.images.map((image: Image, index: number) => (
+                    <div key={index} className="col-12">
+                      <LoadingImage
+                        imageUri={image.image}
+                        className="img-fluid shadow-lg mb-5 bg-body rounded w-100"
+                      />
                     </div>
-                  </div>
+                  ))}
                 </div>
               </div>
             </div>
